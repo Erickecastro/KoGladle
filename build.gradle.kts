a7047737acf3dd2ad01669cbad9e4b59685c8d8b
Ericke Castro <eerickevh@gmail.com>
2026-06-23 21:05:04 -0400

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin(
+        "jvm"
+    ) version "2.3.21"
+}
+
+group =
+    "org.example"
+version =
+    "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(
+        kotlin(
+            "test"
+        )
+    )
+}
+
+kotlin {
+    jvmToolchain(
+        21
+    )
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
